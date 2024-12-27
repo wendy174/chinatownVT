7448eb67f3709d66c9b233907396406c3d35cabd
wendy <mizukilee@gmail.com>
2024-12-27 12:04:39 -0500

    menu items sorting

--- backend/index.js
+++ backend/index.js
@@ -37,12 +37,56 @@ app.use(cors({ origin: 'http://localhost:5173' }));
 
 const categoryOrder = ["Appetizers", "Soup", "Egg Foo Young", "Lo Mein", "Chop Suey", "Fried Rice", "Chow Mein", "Vegetables", "Sweet & Sour", "Poultry", "Pork", "Beef", "Seafood", "House Specialties", "Special Diet Menu", "Combination Plates", "Lunch" ]
 
-// returns menu sorted by category
+
+// ensures options are right next to each other ex c3 & c3a
 app.get('/menu', async (req, res) => {
   try {
     let menuItems = await mongoose.connection.db
       .collection('menuItems')
       .aggregate([
+        {
+          $addFields: {
+            menuIdNumeric: {
+              $toInt: {
+                $ifNull: [
+                  {
+                    $getField: {
+                      field: "match",
+                      input: {
+                        $arrayElemAt: [
+                          { $regexFindAll: { input: "$menu_id", regex: /[0-9]+/ } }, 0
+                        ]
+                      }
+                    }
+                  },
+                  "0"
+                ]
+              }
+            },
+            menuIdAlpha: {
+              $ifNull: [
+                {
+                  $getField: {
+                    field: "match",
+                    input: {
+                      $arrayElemAt: [
+                        { $regexFindAll: { input: "$menu_id", regex: /[a-zA-Z]+/ } }, 0
+                      ]
+                    }
+                  }
+                },
+                ""
+              ]
+            }
+          }
+        },
+        {
+          $sort: {
+            category: 1,
+            menuIdNumeric: 1,
+            menuIdAlpha: 1
+          }
+        },
         {
           $group: {
             _id: "$category",
@@ -52,7 +96,6 @@ app.get('/menu', async (req, res) => {
       ])
       .toArray();
 
-    // Custom sort by predefined array
     menuItems.sort((a, b) => {
       return categoryOrder.indexOf(a._id) - categoryOrder.indexOf(b._id);
     });
@@ -63,6 +106,37 @@ app.get('/menu', async (req, res) => {
     res.status(500).send('Error fetching menu items');
   }
 });
+
+
+
+// Menu sorted by category in certain order 
+
+// returns menu sorted by category
+// app.get('/menu', async (req, res) => {
+//   try {
+//     let menuItems = await mongoose.connection.db
+//       .collection('menuItems')
+//       .aggregate([
+//         {
+//           $group: {
+//             _id: "$category",
+//             items: { $push: "$$ROOT" }
+//           }
+//         }
+//       ])
+//       .toArray();
+
+//     // Custom sort by predefined array
+//     menuItems.sort((a, b) => {
+//       return categoryOrder.indexOf(a._id) - categoryOrder.indexOf(b._id);
+//     });
+
+//     res.status(200).json(menuItems);
+//   } catch (error) {
+//     console.error('Aggregation Error:', error);
+//     res.status(500).send('Error fetching menu items');
+//   }
+// });
 
 
 
