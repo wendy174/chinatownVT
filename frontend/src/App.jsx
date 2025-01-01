a07c7fbd2f80816afe4e10eda3f338885c2d4002
wendy <mizukilee@gmail.com>
2024-12-31 23:00:44 -0500

    Welcome page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import { useState, useEffect } from 'react'; 
 import './App.css'; 
 import MenuPage from './pages/MenuPage'; 
 import ScrollToTop from './components/ScrollToTop'; 
+import HomePage from './pages/HomePage'
 
 function App() {
 
   return (
-    <>
-     <MenuPage />
-     <ScrollToTop />
-    </>
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/menu' element={<MenuPage />} />
+      </Routes>
+    </Router>
+
   )
 }
 
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import Welcome from '../components/Welcome'; 
+
+export default function HomePage() { 
+
+    return ( 
+        <div>
+            <Welcome />
+        </div>
+    )
+}
