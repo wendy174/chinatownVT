a0716e40dc704dfbd40c996750d9b2040d6335c8
wendy <mizukilee@gmail.com>
2025-01-02 22:17:51 -0500

    created about page and added background picture

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -2,16 +2,24 @@ import chinatown from '../assets/chinatown.webp';
 
 export default function Welcome() { 
     return ( 
-        <div className='grid grid-cols-1 lg:grid-cols-2 items-center min-h-screen'>
-            <div className='flex justify-center lg:justify-start'>
-                <img src={chinatown} alt='Chinatown' className='max-w-xl lg:max-w-3xl w-full h-auto'/>
-            </div>
+        <div className="flex justify-center items-center min-h-screen">
+            <div className='grid grid-cols-1 lg:grid-cols-2 items-center bg-white bg-opacity-95 shadow-xl rounded-xl max-w-7xl w-full mx-6 lg:mx-12 p-10 lg:p-16'>
+                <div className='flex justify-center lg:justify-start'>
+                    <img 
+                        src={chinatown} 
+                        alt='Chinatown' 
+                        className='max-w-xl lg:max-w-3xl w-full h-auto rounded-lg'
+                    />
+                </div>
 
-            <div className='px-12 lg:px-20 text-center'	>
-                <h1 className='text-5xl font-bold mb-8'>Your neighborhood Chinese restaurant</h1>
-                <h1 className='text-3xl text-gray-700 leading-relaxed'>
-                    Chinese food brings friends and family together, creating unforgettable moments around the table.
-                </h1>
+                <div className='px-8 lg:px-12 text-center'>
+                    <h1 className='text-5xl font-bold text-gray-800 mb-6 leading-snug'>
+                        Your neighborhood Chinese restaurant
+                    </h1>
+                    <h2 className='text-2xl text-gray-600 leading-relaxed'>
+                        Chinese food brings friends and family together, creating unforgettable moments around the table.
+                    </h2>
+                </div>
             </div>
         </div>
     );
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import Welcome from '../components/Welcome'; 
 import Navbar from '../components/Navbar'; 
+import AboutUs from '../components/Aboutus'; 
 
 export default function HomePage() { 
-
     return ( 
-        <div>
+        <div >
             <Navbar />
             <Welcome />
+            <AboutUs />
         </div>
     )
 }
