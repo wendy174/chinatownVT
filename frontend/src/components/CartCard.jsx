015ff8fc960de5343ab0089258ed14867d4ebda4
wendy <mizukilee@gmail.com>
2025-01-19 21:28:24 -0500

    enabled shopping card --> addToCart

--- frontend/src/components/CartCard.jsx
+++ frontend/src/components/CartCard.jsx
@@ -1,3 +1,5 @@
+import { ShopContext } from '../context/shop-context'
+import { useContext } from 'react'; 
 import {
     Card,
     CardDescription,
@@ -6,15 +8,20 @@ import {
   } from "@/components/ui/card";
 
 
-export default function CartCard() { 
+
+
+export default function CartCard({item}) { 
+
+    const price = item.prices.default || item.prices.small || item.prices.large
+    const formattedPrice = price.toFixed(2);
 
     return (
         <div>
-             <Card>
+              <Card>
                     <CardHeader>
-                        <CardTitle> Id. name. spice</CardTitle>
-                        <CardDescription>Description </CardDescription>
-                        <CardDescription>Price</CardDescription>
+                        <CardTitle>{item.menu_id}. {item.name} {item.isSpicy && <span className="spicy-tag">🌶️</span>}</CardTitle>
+                        <CardDescription>{item.descriptions.item} </CardDescription>
+                        <CardDescription>${formattedPrice}+</CardDescription>
                     </CardHeader>
                 </Card>
             
--- frontend/src/context/shop-context.jsx
+++ frontend/src/context/shop-context.jsx
@@ -1,29 +1,23 @@
-
 import { createContext, useState } from "react"; 
-
 export const ShopContext = createContext(null); 
 
 export default function ShopContextProvider(props) { 
 
     const [cartItems, setCartItems] = useState([]); 
 
+    // When you want to use the most current state --> pass in function --> this is known as functional form
 
+    // Use functional form to ensure your getting most current state 
+        // multiple batch updates in quick succession might lead the function to use an old state because react batch state updates 
     function addToCart(item) {
         setCartItems((prevCart) => [...prevCart, item])
-        console.log(cartItems)
     }
 
-
-    const contextValue = { cartItems, addToCart}
-
+    const contextValue = { cartItems, addToCart }
 
    return ( 
     <ShopContext.Provider value={contextValue}>
         {props.children}
     </ShopContext.Provider>
    )
-
-
-
-   
 }
