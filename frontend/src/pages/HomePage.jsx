c0ec81fb5f6f2e5bd076153e2545c01b79a1a1b7
wendy <mizukilee@gmail.com>
2025-01-19 22:23:26 -0500

    updated CustomMap to ContactUs and reorganized ContactUs

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,21 @@
 import Welcome from '../components/Welcome'; 
 import Navbar from '../components/Navbar'; 
 import AboutUs from '../components/AboutUs'; 
+import { APIProvider } from "@vis.gl/react-google-maps";
+import ContactUs from '../components/ContactUs'; 
 
 export default function HomePage() { 
+
+    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY; 
+
     return ( 
         <div >
             <Navbar />
             <Welcome />
             <AboutUs />
+            <APIProvider apiKey={apiKey}>
+              <ContactUs />
+            </APIProvider>
         </div>
     )
 }
