4680a46bef2c469c895b83eb1c23e0509c569670
wendy <mizukilee@gmail.com>
2025-03-06 12:46:52 -0500

    Added Clear Cart button in cart

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,8 +1,19 @@
 import CartList from "../components/cart/CartList";
 import Navbar from "../components/Navbar";
 import CartTotalCost from "../components/cart/CartTotalCost"; 
+import { Button } from "@/components/ui/button"; 
+import { ShopContext } from '../context/shop-context'; 
+import { useContext } from 'react'; 
+
 
 export default function CartPage() {
+
+  const { setCartItems } = useContext(ShopContext); 
+
+  const handleClearCart = () => { 
+    setCartItems([])
+  }
+
   return (
     <div className="flex flex-col items-center">
       {/* Navbar */}
@@ -10,11 +21,16 @@ export default function CartPage() {
 
       {/* Page Content */}
       <div className="w-full max-w-5xl bg-white shadow-lg rounded-lg py-8 mt-6 px-5 ">
-        <h1 className="text-2xl font-bold text-left">
-          Cart
-        </h1>
-        <CartList />
+        
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl font-bold text-left">
+            Cart
+          </h1>
+          <Button variant="secondary" onClick={() => handleClearCart()}>Clear Cart</Button>
+        </div>
+  
 
+        <CartList />
         <CartTotalCost />
 
    
